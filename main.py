e5efe338298275a253674242fdccd3580a9bae4c
rezaur rahman <rezaur.rahman@bjitgroup.com>
2024-09-02 20:23:38 +0600

    "main.py modified"

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name, id_no):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {id_no}')
 
 
 # Press the green button in the gutter to run the script.
